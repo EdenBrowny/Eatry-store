9d2c2d9e85dc241c3b6f11f90afcd527e4a2fe3d
orisa07037616291 <orisakwepeter@gmail.com>
2022-03-16 14:27:32 +0100

    commit

--- Eatry.js
+++ Eatry.js
@@ -13,21 +13,21 @@ window.onscroll = () => {
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
 
-  section.forEach((sec) => {
-    let top = window.scrollY;
-    let height = sec.offsetHeight;
-    let offset = sec.offsetTop - 150;
-    let id = sec.getAttribute("id");
+  // section.forEach((sec) => {
+  //   let top = window.scrollY;
+  //   let height = sec.offsetHeight;
+  //   let offset = sec.offsetTop - 150;
+  //   let id = sec.getAttribute("id");
 
-    if ((top) => offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header .navbar a[href*=" + id + "]")
-          .classList.add("active");
-      });
-    }
-  });
+  //   if ((top) => offset && top < offset + height) {
+  //     navLinks.forEach((links) => {
+  //       links.classList.remove("active");
+  //       document
+  //         .querySelector("header .navbar a[href*=" + id + "]")
+  //         .classList.add("active");
+  //     });
+  //   }
+  // });
 };
 
 document.querySelector("#search-icon").onclick = () => {
